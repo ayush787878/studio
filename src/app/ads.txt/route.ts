5ff0e77b1bf9372906aead920dd860f339b4543e
japear seller <japearseller@gmail.com>
2025-07-07 19:31:36 +0000

    We couldn't verify your site. Make sure that the changes you made to you

---
+++ src/app/ads.txt/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const adsTxtContent = `google.com, pub-9124896176133484, DIRECT, f08c47fec0942fa0`;
+  
+  return new NextResponse(adsTxtContent, {
+    headers: {
+      'Content-Type': 'text/plain; charset=UTF-8',
+    },
+  });
+}
