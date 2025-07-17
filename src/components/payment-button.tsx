e00dc97ca30b1ede5cdb20281530e65b2be81cdd
japear seller <japearseller@gmail.com>
2025-07-17 20:11:06 +0000

    form is not worign

--- src/components/payment-button.tsx
+++ src/components/payment-button.tsx
@@ -11,47 +11,22 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({ formHtml }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (containerRef.current) {
-      const container = containerRef.current;
-      container.innerHTML = ''; // Clear previous content
-
-      const template = document.createElement('template');
-      template.innerHTML = formHtml.trim();
-      const fragment = template.content;
-
-      // Find the script node within the fragment
-      const scriptNode = fragment.querySelector('script');
+    const container = containerRef.current;
+    if (container) {
+      container.innerHTML = formHtml;
       
-      // Remove the script from the fragment before appending the rest
-      if (scriptNode) {
-        scriptNode.remove();
-      }
-
-      // Append the HTML part (form, style, etc.)
-      container.appendChild(fragment);
-
-      // If a script tag was found, create a new script element and append it to the container
-      if (scriptNode) {
-        const script = document.createElement('script');
-        script.src = scriptNode.src;
-        
-        // Copy all attributes from the original script to the new one
-        for (const attr of scriptNode.attributes) {
-            script.setAttribute(attr.name, attr.value);
+      const script = container.querySelector('script');
+      if (script) {
+        const newScript = document.createElement('script');
+        newScript.src = script.src;
+        for (const attr of script.attributes) {
+          newScript.setAttribute(attr.name, attr.value);
         }
-        if (scriptNode.hasAttribute('async')) {
-            script.async = true;
+        if (script.hasAttribute('async')) {
+            newScript.async = true;
         }
         
-        // Append the script to the container so it executes in the right context
-        container.appendChild(script);
-
-        // Cleanup the script when the component unmounts
-        return () => {
-          if (container.contains(script)) {
-            container.removeChild(script);
-          }
-        };
+        script.parentNode?.replaceChild(newScript, script);
       }
     }
   }, [formHtml]);
